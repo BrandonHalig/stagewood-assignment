f138a84b7fc2386e9765f44cd0a9f7707314f371
BrandonHalig <98606932+BrandonHalig@users.noreply.github.com>
2022-05-11 15:17:04 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
 import React from 'react';
 import './App.css';
-import SearchResults from "./components/SearchResults.jsx"
-import { SearchListRoot } from '.';
 import EventCard from './components/EventCard.jsx';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SearchBar from './components/SearchBar.jsx';
-
-let curSearch = '';
-let curSort = 'A-Z';
-let pageNum = new URLSearchParams(window.location.search).get('pageNumber') !== null ? new URLSearchParams(window.location.search).get('pageNumber') : 1;
-
-//update() get's called on page load by SearchBar, when the search bar input is updated, when the sort list updated, or when the page is updated.
-//update() renders the list of event data based on the search query 
-export function update(){
-    setTimeout(function(){
-        if (document.getElementById('inputbar').value !== curSearch || curSort !== document.getElementById("sort").value || 
-        (new URLSearchParams(window.location.search).get('pageNumber') !== pageNum && new URLSearchParams(window.location.search).get('pageNumber') !== null)){
-            pageNum = new URLSearchParams(window.location.search).get('pageNumber') !== null ? new URLSearchParams(window.location.search).get('pageNumber') : 1;
-            curSearch = document.getElementById('inputbar').value;
-            curSort = document.getElementById("sort").value;
-            SearchListRoot.render(
-                <SearchResults />
-            );
-        }
-    }, 100);
-}
 
 function App() {
     return (
